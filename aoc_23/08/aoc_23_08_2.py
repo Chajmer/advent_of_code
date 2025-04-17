9dc598e8cbc7e0063bff241be1ed801ba44c6961
Chajmer <ofbeno.beno@gmail.com>
2025-04-18 00:31:38 +0200

    Refactor aoc_23 day 8 - part 1 little and part 2 - python.

--- aoc_23/08/aoc_23_08_2.py
+++ aoc_23/08/aoc_23_08_2.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+from math import gcd
 
 # =============== SOLUTION PART =============== #
+
+def lcm(a, b): # least common multiple
+    return abs(a*b) // gcd(a, b)
 
 def solve(input):
     # make node map
@@ -10,15 +14,28 @@ def solve(input):
         key, val_info = line.split(' = ')
         left, right = val_info.split(', ')
         map_nodes[key] = (left[1::], right[:-1:])
+    # for cycle endings 'A' and for each:
     # while not final node found iterate through instructions
-    steps = 0
-    current = 'AAA'
-    while current != 'ZZZ':
-        for dir in literal:
-            steps += 1
-            current = map_nodes[current][0] if dir == 'L'\
-                else map_nodes[current][1]
-    return steps
+    steps_arr = []
+    for current in map_nodes.keys():
+        if (current[2] != 'A'):
+            continue
+        steps = 0
+        end_flag = False
+        while not end_flag:
+            for dir in literal:
+                steps += 1
+                current = map_nodes[current][0] if dir == 'L'\
+                    else map_nodes[current][1]
+                if (current[2] == 'Z'):
+                    steps_arr.append(steps)
+                    end_flag = True
+                    break
+    # calculate lcm of all step counts
+    res = steps_arr[0]
+    for steps in steps_arr:
+        res = lcm(res, steps)
+    return res
 
 
 # =============== TEMPLATE PART =============== #
@@ -35,8 +52,6 @@ input_dir = Path(__file__).parent / 'inputs'
 
 # Iterate over all files matching the pattern 'input_*.txt'
 for input_file in sorted(input_dir.glob(f'input_*.txt')):
-    if (input_file.stem[6:] == '4'):
-        continue
     print(f"Input {input_file.stem[6:]}:")
     print(solve(read_input(input_file)))
 
