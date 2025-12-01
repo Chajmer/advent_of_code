bbd23d05f7dc600fc0fe9ae0531f175fdfb0bbad
Chajmer <ofbeno.beno@gmail.com>
2025-12-01 06:57:24 +0100

    Day 1 aoc 25.

---
+++ aoc_25/01/aoc_25_01_1.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+# =============== SOLUTION PART =============== #
+
+def solve(input):
+    result = 0
+    dial = 50
+
+    for line in input.split('\n'):
+        
+        num = int(line[1:])
+
+        if line[0] == 'L':
+            dial -= num
+        else:
+            dial += num
+
+        dial %= 100
+
+        if dial == 0:
+            result += 1
+
+    return result
+
+
+# =============== TEMPLATE PART =============== #
+
+# README INPUTS:
+# Inputs are separete files in this path-name template:
+# {this_file_dir}/inputs/input_*.txt
+
+def read_input(file_name):
+    with open(file_name, 'r') as file:
+        return file.read().rstrip()
+    
+input_dir = Path(__file__).parent / 'inputs'
+
+# Iterate over all files matching the pattern 'input_*.txt'
+for input_file in sorted(input_dir.glob(f'input_*.txt')):
+    print(f"Input {input_file.stem[6:]}:")
+    print(solve(read_input(input_file)))
+
+print("Test done")
